b00abddab48cd3a834ce6dd351e5d1d48d932c3f
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2020-12-05 16:13:39 -0300

    Encript password and refactor login

---
+++ src/main/java/com/facensgammingstore/gammingstore/resources/LoginResource.java
@@ -0,0 +1,26 @@
+package com.facensgammingstore.gammingstore.resources;
+
+import com.facensgammingstore.gammingstore.entities.Login;
+import com.facensgammingstore.gammingstore.entities.User;
+import com.facensgammingstore.gammingstore.services.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/login")
+public class LoginResource {
+
+    @Autowired
+    private LoginService services;
+
+    public ResponseEntity<User> login(@RequestBody Login obj) {
+        User user = services.LoginAuthentication(obj.getEmail(), obj.getPassword());
+
+        return ResponseEntity.ok().body(user);
+    }
+
+}
